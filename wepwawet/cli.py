70a4a13e90e81a6004148ed2ce1f6444824586dd
LALLEMENT Jaufré <J.LALLEMENT@gds.local>
2022-06-23 10:50:16 +0200

    Edit: tweaking output param

--- wepwawet/cli.py
+++ wepwawet/cli.py
@@ -23,41 +23,11 @@ from docopt import docopt
 
 from wepwawet.banner import banner
 from wepwawet.utils.convertions import seconds_to_str
+from wepwawet.utils.stdouthook import StdOutHook
 from wepwawet.utils.color_print import ColorPrint
 import wepwawet.commands
 
 from . import __version__ as VERSION
-
-class StdOutHook:
-  lines = []
-  filename = ""
-
-  def __init__(self, filename, silent, output):
-    self.filename = filename
-    self.silent = silent
-    self.output = output
-
-  def write(self, text, override=False, **kwargs):
-    if not self.silent or override:
-      sys.__stdout__.write(text)
-    self.lines.append(text)
-
-  def write_out(self):
-    if self.output:
-      with open(self.filename, "w") as file:
-        for line in self.lines:
-          # remove stdout colors
-          line = line.replace('\033[91m', '')
-          line = line.replace('\033[92m', '')
-          line = line.replace('\033[93m', '')
-          line = line.replace('\033[94m', '')
-          line = line.replace('\033[95m', '')
-          line = line.replace('\033[0m', '')
-          file.write(line)
-
-  def flush(self):
-    # python3 compatability, does nothing
-    pass
 
 def main():
 
@@ -71,7 +41,8 @@ def main():
     options = docopt(__doc__, version=VERSION)
 
     if options["--output"] or options['--silent']:
-      sys.stdout = StdOutHook(options["FILENAME"], options['--silent'], options['--output'])
+      sys.stdout = StdOutHook(options["FILENAME"], options['--silent'],
+                              options['--output'])
 
     if not options["--target"] and not options['--file']:
       ColorPrint.red(
