c587ee8bfff8b49130f2f3f279568a5a025022c1
LALLEMENT Jaufré <J.LALLEMENT@gds.local>
2022-06-24 17:47:27 +0200

    Edit: implemented more properties from shodan request

--- wepwawet/scanners/shodan.py
+++ wepwawet/scanners/shodan.py
@@ -1,12 +1,11 @@
 import shodan
 
-from wepwawet.utils.color_print import ColorPrint
+sho_props = ["city", "country_name", "domains", "isp", "org", "ports"]
+sho_data_props = ["product"]
 
-sho_props = ['city', 'country_name', 'domains', 'isp', 'org', 'ports']
-
-'''
+"""
 Main shodan function : Emit request to shodan via API
-'''
+"""
 def ask_shodan(self):
   print("Asking Shodan.io for additional information...")
 
@@ -21,15 +20,30 @@ def ask_shodan(self):
   for i in range(len(self.options["TARGET"])):
     target = self.options["TARGET"][i]
     sho_req = None
+    err_msg = ""
 
     try:
       # Asking shodan for the specified IP address
-      if (target['ip']):
-        sho_req = api.host(target['ip'])
+      if (target["ip"]):
+        sho_req = api.host(target["ip"])
 
     except Exception as e:
-      print(e)
+      err_msg = ""
       self.handle_exception(e, f"Error while retreiving shodan informations for {target['host']}")
 
-    sho_res = {x:sho_req[x] if sho_req else None for x in sho_props}
-    self.urls.append({ **target, **sho_res })
+    sho_base = filter_dictionary_properties(sho_req, sho_props)
+    sho_data = filter_nested_properties(sho_req["data"], sho_data_props)
+    
+    self.urls.append({
+      **target,
+      **sho_base,
+      **sho_data,
+      "error": err_msg
+    })
+
+
+def filter_dictionary_properties(dictionary, properties):
+  return { x:dictionary[x] if dictionary[x] else "" for x in properties }
+
+def filter_nested_properties(dictionaries, properties):
+  return { p: [ d[p] for d in dictionaries ] for p in properties }
