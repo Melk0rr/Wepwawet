da4e7a1483c05cc28d816437bc030a1ddbd9d17f
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-09-21 15:42:13 +0200

    Edit: added several print in order to improve feedback during execution

--- wepwawet/scanners/http.py
+++ wepwawet/scanners/http.py
@@ -1,13 +1,17 @@
-import requests
+""" HTTP plugin used to check basic http infos for a given target """
 import re
+import requests
 from bs4 import BeautifulSoup
 
 def http_info(self, target):
+  """ Performs a simple HTTP request to the given target """
   host = target["host"]
   if not re.match(r'http(s?):', host):
     host = 'http://' + target["host"]
-  
-  http_status = http_title = ""  
+
+  http_status = http_title = ""
+
+  print(f"\nRequesting {host}...")
 
   try:
     req = requests.get(host)
@@ -16,12 +20,14 @@ def http_info(self, target):
     http_status = req.status_code
     http_title = soup.title.text
 
+    print(f"HTTP status: {http_status}, HTTP title: {http_title}")
+
   except Exception as e:
     self.handle_exception(e, f"Error while requesting {host}. Make sure the target is accessible")
 
   target_index = self.options["TARGET"].index(target)
 
-  self.urls[target_index] = { 
+  self.urls[target_index] = {
     **self.urls[target_index],
     "http_status": http_status,
     "http_title": http_title
--- wepwawet/scanners/shodan.py
+++ wepwawet/scanners/shodan.py
@@ -1,47 +1,8 @@
 """ Shodan plugin which will interact with Shodan API to retreive ip data """
-from xml.dom import NotFoundErr
 import shodan
+from wepwawet.utils.dictionary_join import join_dictionary_items
 
 sho_props = ["city", "country_name", "domains", "isp", "org", "ports"]
-
-
-def ask_shodan(self):
-  """ Main shodan function : Emit request to shodan via API """
-
-  print("Asking Shodan.io for additional information...")
-
-  try:
-    from wepwawet.API import SHODAN_KEY
-  except NotFoundErr as err:
-    self.handle_exception(err, "Unable to import API key - make sure API.py exists!")
-    return
-
-  api = shodan.Shodan(SHODAN_KEY)
-
-  for i in range(len(self.options["TARGET"])):
-    target = self.options["TARGET"][i]
-    sho_req = {}
-    err_msg = ""
-
-    try:
-      # Asking shodan for the specified IP address
-      if target["ip"]:
-        sho_req = api.host(target["ip"])
-
-    except Exception as err:
-      err_msg = err
-      self.handle_exception(err, f"Error while retreiving shodan informations for {target['host']}")
-
-    sho_base = filter_shodan_properties(sho_req, sho_props)
-    sho_products = get_shodan_product(sho_req)
-
-    self.urls.append({
-      **target,
-      **sho_base,
-      **sho_products,
-      "error": err_msg
-    })
-
 
 def filter_shodan_properties(shodan_req, props):
   """ Filter shodan properties based on shop_props collection """
@@ -64,3 +25,43 @@ def get_shodan_product(shodan_req):
       product_list.add(f"{product_str}{port_str}")
 
   return {'product': ', '.join(product_list)}
+
+
+def ask_shodan(self):
+  """ Main shodan function : Emit request to shodan via API """
+
+  print("Asking Shodan.io...")
+
+  try:
+    from wepwawet.API import SHODAN_KEY
+  except Exception as err:
+    self.handle_exception(err, "Unable to import API key - make sure API.py exists!")
+    return
+
+  api = shodan.Shodan(SHODAN_KEY)
+
+  for i in range(len(self.options["TARGET"])):
+    target = self.options["TARGET"][i]
+    sho_req = {}
+    err_msg = ""
+
+    print(f"\n* {target['host']} *")
+
+    try:
+      # Asking shodan for the specified IP address
+      if target["ip"]:
+        sho_req = api.host(target["ip"])
+
+    except Exception as err:
+      err_msg = err
+      self.handle_exception(err, f"Error while retreiving shodan informations for {target['host']}")
+
+    res = {
+      **target,
+      **filter_shodan_properties(sho_req, sho_props),
+      **get_shodan_product(sho_req),
+      "error": err_msg
+    }
+
+    print(join_dictionary_items(res, "\n"))
+    self.urls.append(res)
