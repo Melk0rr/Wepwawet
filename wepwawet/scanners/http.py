bde5406f3a9bba56246f8c370e98dfd10e83b5be
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-11-18 13:36:56 +0100

    Edit: cleaned http function

--- wepwawet/scanners/http.py
+++ wepwawet/scanners/http.py
@@ -1,14 +1,11 @@
 """ HTTP plugin used to check basic http infos for a given target """
-import re
 import requests
 from bs4 import BeautifulSoup
 
-def http_info(self, target):
+def http_info(self, target_index):
   """ Performs a simple HTTP request to the given target """
+  target = self.urls[target_index]
   host = target["host"]
-  if not re.match(r'http(s?):', host):
-    host = 'http://' + target["host"]
-
   http_status = http_title = ""
 
   print(f"\nRequesting {host}...")
@@ -25,10 +22,8 @@ def http_info(self, target):
   except Exception as e:
     self.handle_exception(e, f"Error while requesting {host}. Make sure the target is accessible")
 
-  target_index = self.options["TARGET"].index(target)
-
   self.urls[target_index] = {
-    **self.urls[target_index],
+    **target,
     "http_status": http_status,
     "http_title": http_title
   }
