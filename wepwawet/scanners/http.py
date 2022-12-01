39fe54cf9b801832f9f1e7e1b7a0ac910a586616
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-12-01 18:07:56 +0100

    Edit: handled missing protocol before doing a http request

--- wepwawet/scanners/http.py
+++ wepwawet/scanners/http.py
@@ -1,29 +1,36 @@
 """ HTTP plugin used to check basic http infos for a given target """
+import re
 import requests
 from bs4 import BeautifulSoup
+
 
 def http_info(self, target_index):
   """ Performs a simple HTTP request to the given target """
   target = self.urls[target_index]
-  host = target["host"]
-  http_status = http_title = ""
 
-  print(f"\nRequesting {host}...")
+  host = target["host"]
+
+  if not re.match(r'http(s?):', host):
+    host = 'http://' + target["host"]
+
+  http_status = http_title = ""
+  print(f"\nRequesting {target['host']}...")
 
   try:
     req = requests.get(host)
     soup = BeautifulSoup(req.content, 'html.parser')
 
-  except Exception as e:
-    self.handle_exception(e, f"Error while requesting {host}. Make sure the target is accessible")
+    http_status = req.status_code
+    http_title = soup.title.text
 
-  http_status = req.status_code
-  http_title = soup.title.text
+  except Exception as e:
+    self.handle_exception(
+        e, f"Error while requesting {target['host']}. Make sure the target is accessible")
 
   print(f"HTTP status: {http_status}, HTTP title: {http_title}")
 
   self.urls[target_index] = {
-    **target,
-    "http_status": http_status,
-    "http_title": http_title
+      **target,
+      "http_status": http_status,
+      "http_title": http_title
   }
