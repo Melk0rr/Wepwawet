a2b67d0831ae158a2aa2a51ba2bdc2c4f6a62b10
Sébastien Cantarell <sebastien.cantarell@gmail.com>
2023-03-27 00:50:57 +0200

    New TLS test

--- wepwawet/utils/url.py
+++ wepwawet/utils/url.py
@@ -35,61 +35,67 @@ class URL:
 
     def Create_Socket(self):
         try:
-            self.l_Socket = socket.create_connection((self.Get_Domain(), self.Get_Port()))
-            self.B_SOCKET_OPENED = True
-            return self.l_Socket
+            if self.B_SOCKET_OPENED:
+                print (__class__.__name__ + ": Socket already opened")
+                return -1
+            else:
+                self.l_Socket = socket.create_connection((self.Get_Domain(), self.Get_Port()))
+                self.B_SOCKET_OPENED = True
+                return self.l_Socket
         except:
-            raise Exception("Error while creating socket")
+            self.B_SOCKET_OPENED = False
+            print (__class__.__name__ + ": Error while creating socket")
+            return -1
 
     def Close_Socket(self):
         if self.B_SOCKET_OPENED:
             self.l_Socket.close();
-        self.B_SOCKET_OPENED = False
-
+            self.B_SOCKET_OPENED = False
+            self.B_SOCKET_WRAPPED = False
+        else:
+            print(f"In {__class__.__name__} : Trying to close an unexisting socket")
+        
 
     def Get_Socket(self):
         if(self.l_Socket!=-1):
             return self.l_Socket
 
-    def Wrap_Socket(self):
+    def Wrap_Socket(self,TLS_version=None):
         try:
-            self.l_SSLcontext = ssl.create_default_context()
-            self.l_WrappedSocket = self.l_SSLcontext.wrap_socket(self.Get_Socket(), server_hostname=self.Get_Domain())
-            self.B_SOCKET_WRAPPED = True
-            return True
-        except:    
-            print("cannot wrap socket with default context")
+            if self.B_SOCKET_WRAPPED:
+                print(f"In {__class__.__name__} : Warning trying to wrap a socket already Wrapped")
+                return False
+            else:
+                if TLS_version != None:
+                    self.l_SSLcontext = ssl.SSLContext(TLS_version or ssl.PROTOCOL_SSLv23)
+                else:
+                    self.l_SSLcontext = ssl.create_default_context()
+                    
+                self.l_WrappedSocket = self.l_SSLcontext.wrap_socket(self.Get_Socket(), server_hostname=self.Get_Domain())
+                self.B_SOCKET_WRAPPED = True
+                return True
+        except Exception as err:
+            print(f"In {__class__.__name__} : {type(err).__name__} cannot wrap socket with default {TLS_version}: {err}")        
+            self.B_SOCKET_WRAPPED = False
             return False
 
-            
-    def Wrap_Socket_With(self,TLS_version):
-        try:
-            self.l_SSLcontext = ssl.SSLContext(TLS_version)
-            self.l_WrappedSocket = self.l_SSLcontext.wrap_socket(self.Get_Socket(), server_hostname=self.Get_Domain())
-            self.B_SOCKET_WRAPPED = True
-            return True
-        except:    
-            print("cannot wrap socket with",TLS_version)
-            return False
-            
     def Get_SSLVersion(self):
         if self.B_SOCKET_WRAPPED:
             return self.l_WrappedSocket.version()
         else:
-            return "Socket was not Wrapped"
+            print(f"In {__class__.__name__} : Socket was not Wrapped")
 
     def Get_SSLCyherUsed(self):
         if self.B_SOCKET_WRAPPED:
             return self.l_WrappedSocket.cipher()
         else:
-            return "Socket is Not Wrapped: No cypher can be used"
-
+            print(f"In {__class__.__name__} : Socket is Not Wrapped: No cypher can be used")
 
     def Get_Certificate(self):
         if self.B_SOCKET_WRAPPED:
             return self.l_WrappedSocket.getpeercert()
         else:
-            return "Socket is Not Wrapped: No certificate can be retrieved"
+            print(f"In {__class__.__name__} : Socket is Not Wrapped: No certificate can be retrieved")
 
     def Get_Header(self):
         if self.B_SOCKET_WRAPPED:
@@ -98,11 +104,19 @@ class URL:
             self.l_WrappedSocket.sendall(str_Encoded)
             return self.l_WrappedSocket.recv(1024).split(b"\r\n")
         else:
-            return "Socket is Not Wrapped: No certificate can be retrieved"
+            print(f"In {__class__.__name__} : ERROR Socket is Not Wrapped: No certificate can be retrieved")
 
+    def Get_SSLContext(self):
+        if(self.l_SSLcontext!=-1):
+            return self.l_SSLcontext
 
-
-
-
+    def IsTLSEnabled(self,TLS_version):
+        try:
+            self.Create_Socket()
+            value = self.Wrap_Socket(TLS_version)
+            self.Close_Socket()
+            return value
+        except:
+            return False
 
 
