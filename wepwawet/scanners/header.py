ef7d71a77b6b0819a8aec7143f04217a0cd08b1b
Jaufré Lallement <runner-me1k0r@proton.me>
2024-02-01 11:38:16 +0100

    Edit: skip header and tls check if url ip is not resolved

--- wepwawet/scanners/header.py
+++ wepwawet/scanners/header.py
@@ -5,50 +5,56 @@ from wepwawet.utils.color_print import ColorPrint
 
 def check_header(target):
   """ Main Header function : Retrieve Header """
-  print(
-      f"Cheking Header for {target.get_domain()} with best SSL connection available")
-  local_ssl_socket = SSLSocket(url=target)
-  local_ssl_socket.open_socket()
+  response = {}
+
+  if target.get_ip():
+    print(f"{target.get_domain()}: checking header with best SSL connection available", end="...")
+    local_ssl_socket = SSLSocket(url=target)
+    local_ssl_socket.open_socket()
 
 
-  try:
-    if local_ssl_socket.wrap_ssl_socket(tls_version=None):
-      local_ssl_socket.get_header()
-      if local_ssl_socket.ssl_certificate.state:
-        local_ssl_socket.get_certificate()
+    try:
+      if local_ssl_socket.wrap_ssl_socket(tls_version=None):
+        local_ssl_socket.get_header()
+        if local_ssl_socket.ssl_certificate.state:
+          local_ssl_socket.get_certificate()
 
-      try:
-        local_ssl_socket.header.analyse()
-      except Exception as e:
-        ColorPrint.red(f"{e} : Error while analysing Header of {target.get_domain()}")
+        try:
+          local_ssl_socket.header.analyse()
+        except Exception as e:
+          ColorPrint.red(f"{e} : Error while analysing Header of {target.get_domain()}")
 
-      try:
-        local_ssl_socket.ssl_certificate.analyse()
-      except Exception as e:
-        ColorPrint.red(f"{e} : Error while analysing certificate of {target.get_domain()}")
-      
+        try:
+          local_ssl_socket.ssl_certificate.analyse()
+        except Exception as e:
+          ColorPrint.red(f"{e} : Error while analysing certificate of {target.get_domain()}")
+        
 
-  except Exception as e:
-    ColorPrint.red(
-        f"{e} : Error while retrieving Header informations for {target.get_domain()}")
- 
+    except Exception as e:
+      ColorPrint.red(
+          f"{e} : Error while retrieving Header informations for {target.get_domain()}")
+  
 
 
-  response = {
-      "Cert State":  local_ssl_socket.ssl_certificate.state,
-      "Cert code": local_ssl_socket.ssl_certificate.code,
-      "Cert reason": local_ssl_socket.ssl_certificate.reason,
-      "Cert message": local_ssl_socket.ssl_certificate.message,
-      "Cert validity": local_ssl_socket.ssl_certificate.validity,
-      "Cert CN": local_ssl_socket.ssl_certificate.issued_for_cn,
-      "Cert O": local_ssl_socket.ssl_certificate.issued_for_o,
-      "Certificate": local_ssl_socket.ssl_certificate.data,
-      "Header csp": local_ssl_socket.header.csp,
-      "Header hsts": local_ssl_socket.header.hsts,
-      "Header x-frame": local_ssl_socket.header.x_frame,
-      "Header x-content": local_ssl_socket.header.x_content,
-      "Header": local_ssl_socket.header.data
-  }
+    response = {
+        "Cert State":  local_ssl_socket.ssl_certificate.state,
+        "Cert code": local_ssl_socket.ssl_certificate.code,
+        "Cert reason": local_ssl_socket.ssl_certificate.reason,
+        "Cert message": local_ssl_socket.ssl_certificate.message,
+        "Cert validity": local_ssl_socket.ssl_certificate.validity,
+        "Cert CN": local_ssl_socket.ssl_certificate.issued_for_cn,
+        "Cert O": local_ssl_socket.ssl_certificate.issued_for_o,
+        "Certificate": local_ssl_socket.ssl_certificate.data,
+        "Header csp": local_ssl_socket.header.csp,
+        "Header hsts": local_ssl_socket.header.hsts,
+        "Header x-frame": local_ssl_socket.header.x_frame,
+        "Header x-content": local_ssl_socket.header.x_content,
+        "Header": local_ssl_socket.header.data
+    }
 
-  local_ssl_socket.close_socket()
+    local_ssl_socket.close_socket()
+
+  else:
+    print(f"{target.get_domain()}: IP not resolved. Skipping...")
+
   return response
--- wepwawet/scanners/tls.py
+++ wepwawet/scanners/tls.py
@@ -4,17 +4,21 @@ from wepwawet.utils.color_print import ColorPrint
 
 def check_tls(target):
   """ Main TLS function : Check TLS """
-  print(f"Cheking TLS for {target.get_domain()}")
-
-  local_ssl_socket = SSLSocket(url=target)
-
   tls_response = {}
 
-  try:
-    tls_response = local_ssl_socket.get_tls_state()
+  if target.get_ip():
+    print(f"Cheking TLS for {target.get_domain()}")
 
-  except Exception as e:
-    ColorPrint.red(
-        f"{e} : Error while checking TLS informations for {target.get_domain()}")
+    local_ssl_socket = SSLSocket(url=target)
+
+    try:
+      tls_response = local_ssl_socket.get_tls_state()
+
+    except Exception as e:
+      ColorPrint.red(
+          f"{e} : Error while checking TLS informations for {target.get_domain()}")
+
+  else:
+    print(f"{target.get_domain()}: IP not resolved. Skipping...")
 
   return tls_response
