759253f42c6071b381ea8c41a78ded59b2d01e5c
Me1k0r <runner-me1k0r@proton.me>
2025-01-15 15:23:32 +0100

    edit: scanners - nmap fix handling when target has no ip + implemented changes to port

--- wepwawet/scanners/nmap.py
+++ wepwawet/scanners/nmap.py
@@ -1,5 +1,7 @@
 import nmap3
 
+from typing import Dict
+
 from wepwawet.network.port import Port
 
 nmap = nmap3.Nmap()
@@ -15,25 +17,32 @@ nmap_commands = {
 def set_target_ports(target, nmap_res):
   """ Set target ports based on nmap result """
 
-  nmap_ip_res = nmap_res.get(target.get_ip().get_address(), {})
+  nmap_ip_res = nmap_res.get(str(target.get_ip()), {})
   for p in nmap_ip_res.get("ports", []):
     product = p["service"].get("product", p["service"].get("name", "Unknown"))
     port = Port(int(p["portid"]), product, p.get("state", "opened"))
     target.get_ip().append_open_port(port)
 
-def nmap(self, target, command = "ver-detect"):
+def nmap(self, target: "URL", command: str = "ver-detect"):
   """ Perform an nmap scan on the target """
 
   if command not in nmap_commands.keys():
     raise ValueError(f"{command} is not a valid nmap command. Please use one of the following commands: \n{nmap_commands.keys()}")
 
-  print(f"{target.get_domain()}, scanning with nmap", end="...")
-  nmap_res = nmap_commands[command](target.get_ip().get_address())
+  nmap_res = None
 
-  if command == "ver-detect":
-    set_target_ports(target, nmap_res)
-    target_ports = target.get_ip().get_port_strings()
-    target_ports_list = "\n".join(target_ports)
-    print(f"Found {len(target_ports)} ports on {target.get_domain()}:\n{target_ports_list}")
+  if target.get_ip() is not None:
+    print(f"Scanning {target.get_domain()} with nmap", end="...")
+
+    nmap_res = nmap_commands[command](target.get_ip().get_address())
+
+    if command == "ver-detect":
+      set_target_ports(target, nmap_res)
+      target_ports = target.get_ip().get_port_strings()
+      target_ports_list = "\n".join(target_ports)
+      print(f"Found {len(target_ports)} ports on {target.get_domain()}:\n{target_ports_list}")
+  
+  else:
+    print(f"No ip to scan for {target.get_domain()}")
 
   return nmap_res
