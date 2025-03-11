1c20a110f5f3c998ad6b2d3e9cfa1cc9f87934be
Me1k0r <runner-me1k0r@proton.me>
2025-03-11 14:45:24 +0100

    edit: scanners/http - formatting + no redirect
    Some formatting + no redirect option

--- wepwawet/scanners/http.py
+++ wepwawet/scanners/http.py
@@ -1,30 +1,31 @@
-""" HTTP plugin used to check basic http infos for a given target """
-import requests
+from typing import TYPE_CHECKING, Dict
 
-from typing import Dict
+import requests
 from bs4 import BeautifulSoup
 
+if TYPE_CHECKING:
+    from wepwawet.network.url import URL
+
 def http_info(self, target: "URL") -> Dict:
-  """ Performs a simple HTTP request to the given target """
+    """Performs a simple HTTP request to the given target"""
 
-  http_status = http_title = ""
+    http_status = None
+    http_title = None
 
-  print(f"{target.get_domain()}, getting HTTP infos", end="...")
+    print(f"{target.get_domain()}, getting HTTP infos", end="...")
 
-  try:
-    req = requests.get(target.get_host())
-    soup = BeautifulSoup(req.content, 'html.parser')
+    try:
+        req = requests.get(target.get_host(), allow_redirects=False)
+        soup = BeautifulSoup(req.content, "html.parser")
 
-    http_status = req.status_code
-    http_title = soup.title.text.replace("\n", "").replace("\t", " ")
+        http_status = req.status_code
+        http_title = soup.title.text.replace("\n", "").replace("\t", " ")
 
-    print(f"{http_title} ({http_status})")
+        print(f"{http_title} ({http_status})")
 
-  except Exception as e:
-    self.handle_exception(
-        e, f"Error while requesting {target.get_domain()}. Make sure the target is accessible")
+    except Exception as e:
+        self.handle_exception(
+            e, f"Error while requesting {target.get_domain()}. Make sure the target is accessible"
+        )
 
-  return {
-      "http_status": http_status,
-      "http_title": http_title
-  }
+    return {"http_status": http_status, "http_title": http_title}
