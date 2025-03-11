1da58e0a4a918cf33ff7afe479870f39bd1bde82
Me1k0r <runner-me1k0r@proton.me>
2025-03-11 17:46:19 +0100

    edit: overall cleanup 2

--- setup.py
+++ setup.py
@@ -1,34 +1,43 @@
-""" Setup module """
+# INFO: Setup
+
 from os.path import abspath, dirname, join
+
 from setuptools import find_packages, setup
 
 from wepwawet import __version__
 
 this_dir = abspath(dirname(__file__))
 
-with open(join(this_dir, 'README.md'), encoding='utf-8') as f:
-  long_desc = f.read()
+with open(join(this_dir, "README.md"), encoding="utf-8") as f:
+    long_desc = f.read()
 
-with open(join(this_dir, 'requirements.txt'), encoding='utf-8') as f:
-  reqs = f.read().splitlines()
+with open(join(this_dir, "requirements.txt"), encoding="utf-8") as f:
+    reqs = f.read().splitlines()
 
 setup(
-    name='wepwawet',
+    name="wepwawet",
     version=__version__,
-    description='URLs information gathering',
+    description="URLs information gathering",
     long_description=long_desc,
     long_description_content_type="text/markdown",
-    url='https://github.com/Melk0rr/Wepwawet',
-    author='Jaufré Lallement',
-    author_email='runner-me1k0r@proton.me',
-    classifiers=['Intended Audience :: Developers',
-                 'Topic :: Utilities',
-                 'License :: Public Domain',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3.10', ],
-    keywords='cli',
-    packages=find_packages(exclude=['docs', 'tests*']), python_requires='>=3',
+    url="https://github.com/Melk0rr/Wepwawet",
+    author="Jaufré Lallement",
+    author_email="runner-me1k0r@proton.me",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="cli",
+    packages=find_packages(exclude=["docs", "tests*"]),
+    python_requires=">=3",
     install_requires=[reqs],
-    entry_points={'console_scripts': ['wepwawet=wepwawet.cli:main', ], },
+    entry_points={
+        "console_scripts": [
+            "wepwawet=wepwawet.cli:main",
+        ],
+    },
     py_modules=["wepwawet.utils", "wepwawet.scanners"],
 )
